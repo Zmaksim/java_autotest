a1ba0b7486d5702a8e6b81ab74bebe52a7a27e9b
Zmaksim <35296884+Zmaksim@users.noreply.github.com>
2018-01-10 18:19:09 +0300

    Изменение названия

--- sandbox/src/main/java/bb/com/sandbox/helloWorld.java
+++ sandbox/src/main/java/bb/com/sandbox/helloWorld.java
@@ -1,8 +1,7 @@
 package bb.com.sandbox;
 
-public class helloWorld {
-		public static void main (String[] args){
-			
-			System.out.println("Hello World");
-		}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
